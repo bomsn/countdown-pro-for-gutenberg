c7d1ce76599714f303f79ce5a003ffae0d0c0abc
Ali Khallad <bomsn@live.fr>
2022-05-14 17:44:15 +0100

    fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
+
 // Set different CSS extraction for editor only and common block styles
 const blocksCSSPlugin = new MiniCssExtractPlugin({
   filename: './assets/css/blocks.css',
@@ -11,15 +13,17 @@ const editBlocksCSSPlugin = new MiniCssExtractPlugin({
 
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: {
-    './assets/js/blocks.editor': './blocks/index.js',
-    './assets/js/blocks': './blocks/frontend.js',
+    editor: { import: './blocks/index.js', filename: './js/blocks.editor.js' },
+    frontend: { import: './blocks/frontend.js', filename: './js/blocks.js' }
   },
   output: {
-    // path: path.resolve(__dirname),
-    path: __dirname,
-    filename: '[name].js',
+    path: path.resolve(__dirname, 'assets'),
+    filename: '[name]',
+  },
+  resolve: {
+    extensions: ['.js']
   },
   watch: 'production' !== process.env.NODE_ENV,
   devtool: 'eval-cheap-source-map',
@@ -27,7 +31,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
@@ -35,45 +39,92 @@ module.exports = {
       {
         test: /style\.s?css$/,
         use: [
-          { loader: blocksCSSPlugin.loader },
-          { loader: 'raw-loader' },
+          // { loader: blocksCSSPlugin.loader },
+          // { loader: 'raw-loader' },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     plugins: [require('autoprefixer')],
+          //   },
+          // },
+          // "style-loader",
+          // "css-loader",
+          // {
+          //   loader: 'sass-loader',
+          //   options: {
+          //     // Prefer `dart-sass`
+          //     implementation: require("sass"),
+          //     sassOptions: {
+          //       outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
+          //     }
+          //   },
+          // },
+          'style-loader',
           {
-            loader: 'postcss-loader',
+            loader: "css-loader",
             options: {
-              plugins: [require('autoprefixer')],
+              import: true,
+              importLoaders: 1,
+              modules: true
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "postcss-loader",
             options: {
-              sassOptions: {
-                outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
-              }
+              plugins: () => [
+                require("autoprefixer")()
+              ],
             },
           },
+          'sass-loader',
         ],
+        resourceQuery: /raw/,
+        type: 'asset/source',
       },
       {
         test: /editor\.s?css$/,
         use: [
-          { loader: editBlocksCSSPlugin.loader },
-
-          { loader: 'raw-loader' },
+          // { loader: editBlocksCSSPlugin.loader },
+          // { loader: 'raw-loader' },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     plugins: [require('autoprefixer')],
+          //   },
+          // },
+          // "style-loader",
+          // "css-loader",
+          // {
+          //   loader: 'sass-loader',
+          //   options: {
+          //     // Prefer `dart-sass`
+          //     implementation: require("sass"),
+          //     sassOptions: {
+          //       outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
+          //     }
+          //   },
+          // },
+          'style-loader',
           {
-            loader: 'postcss-loader',
+            loader: "css-loader",
             options: {
-              plugins: [require('autoprefixer')],
+              import: true,
+              importLoaders: 1,
+              modules: true
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "postcss-loader",
             options: {
-              sassOptions: {
-                outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
-              }
+              plugins: () => [
+                require("autoprefixer")()
+              ],
             },
           },
+          'sass-loader',
         ],
+        resourceQuery: /raw/,
+        type: 'asset/source',
       },
     ],
   },
